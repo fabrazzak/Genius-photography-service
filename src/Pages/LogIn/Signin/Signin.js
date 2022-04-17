bc1febf3175293b62a309dda5fd75a78f3b5aa0b
fabrazzak <freelancerabdurrazzak47@gmail.com>
2022-04-17 18:20:47 +0600

    Signin pages complet

--- src/Pages/LogIn/Signin/Signin.js
+++ src/Pages/LogIn/Signin/Signin.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Col, Form, InputGroup, Row } from 'react-bootstrap';
+import SocialShare from '../SocialShare/SocialShare';
+import './Signin.css'
 
 const Signin = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
+
+   
     return (
-        <div>
-            <h1>This is Signin pages</h1>
+        <div className="container ">
+            <div className='form mx-auto py-5'>
+                <h1>Please Register!</h1>
+                <form >
+                    <div className="input-groupe">
+                        <input type="text" className='form-control fs-5 border-2' name="text" id="" required placeholder='Your Name' />
+                    </div>
+                    <div className="input-groupe">
+                        <input type="email" className='form-control fs-5 border-2' name="email" id="" required placeholder='Your Email' />
+                    </div>
+                    <div className="input-groupe">
+                        <input type="password" className='form-control fs-5 border-2' name="password" id="" required placeholder='Your Password' />
+                    </div>
+                    <button type="submit" className='btn-lg btn-primary mb-3 px-5 fw-bold fst-italic'> Sign In</button>
+                   
+                </form>
+                <p >Al ready have an account? <span className='text-primary'>Please Login.</span></p>
+                <SocialShare></SocialShare>
+
+            </div>
         </div>
     );
 };
---
+++ src/Pages/LogIn/SocialShare/SocialShare.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialShare = () => {
+    return (
+        <div>
+            <h1>This is social shate</h1>
+        </div>
+    );
+};
+
+export default SocialShare;
