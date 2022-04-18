71e84938384dde9ff1454c843160062a78fd2e8e
fabrazzak <freelancerabdurrazzak47@gmail.com>
2022-04-18 23:16:28 +0600

    about pages update

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -15,9 +15,7 @@ const About = () => {
                         <h4 className='fst-italic text-info '>and more</h4>
                     </div>
                     <p className='mx-lg-5 '>
-                        My name is Abdur Razzak . I’m a photographer specializing in intimate weddings and elopements based out of Southern California. I've always enjoyed the arts and have taken advantage of every chance to engage in them; however, discovering photography in college soon became my passion. Growing up in the pacific northwest, being surrounded by such beautiful landscapes and nature I feel has always inspired me as a photographer, nothing makes me happier than exploring breathtaking scenery with amazing couples. It’s such a privilege to be there to document such important moments in my couple’s lives and it never seems to get old to me, no two weddings or elopements are ever the same, each one is different and unique in a way to reflect that couple's style and personality and I love to document it.
-
-                        My photography style is colorful, candid, romantic, photo-journalistic, true to life, and a little fun. I've been told all my life I have a very laid-back personality and I pride myself on making your day as relaxed and easy-going as possible and most importantly that you enjoy your time spent with me, you have fun, and that you love your photos.</p>
+                        My name is Abdur Razzak.My dream is to build a career as a professional web developer.I'm willing to work as hard as I can to be a full-stack web developer.I am currently working as a WordPress developer at Upwork Marketplace. </p>
                 </div>
             </div>
         </div>
